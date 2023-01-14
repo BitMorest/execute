9fd8c7206a588016bcde0c7f4a2d48f05372f675
John Martin <john.itvn@gmail.com>
2023-01-15 03:30:54 +0700

    feat: support aggregate output mode

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,10 @@ export type Command = {
 	label?: string;
 };
 
+export type ExecOptions = {
+	aggregateOutput?: boolean;
+};
+
 class Executer {
 	readonly resetColor = '\x1b[0m';
 	colors = ['\x1b[32m', '\x1b[33m', '\x1b[34m', '\x1b[35m', '\x1b[36m', '\x1b[37m', '\x1b[90m', '\x1b[31m'];
@@ -37,34 +41,45 @@ class Executer {
 		return commands;
 	}
 
-	execute(command: Command): Promise<void> {
+	execute(command: Command, options?: ExecOptions): Promise<void> {
 		return new Promise((resolve) => {
 			if (command.cwd) {
 				command.cmd = `cd ${command.cwd} && ${command.cmd}`;
 			}
-			const childProcess = exec(command.cmd);
-			childProcess.stdout.on('data', function (data) {
-				process.stdout.write(`${command.label} ${data}`);
-			});
-			childProcess.stderr.on('data', function (data) {
-				process.stderr.write(`${command.label} \x1b[31m${data}\x1b[0m`);
-			});
-			childProcess.on('close', resolve);
+			if (options?.aggregateOutput) {
+				exec(command.cmd, (error, stdout, stderr) => {
+					for (const line of stdout.split('\n')) {
+						process.stdout.write(`${command.label} ${line}\n`);
+					}
+					for (const line of stderr.split('\n')) {
+						process.stderr.write(`${command.label} ${line}\n`);
+					}
+				});
+			} else {
+				const childProcess = exec(command.cmd);
+				childProcess.stdout.on('data', function (data) {
+					process.stdout.write(`${command.label} ${data}`);
+				});
+				childProcess.stderr.on('data', function (data) {
+					process.stderr.write(`${command.label} ${data}`);
+				});
+				childProcess.on('close', resolve);
+			}
 		});
 	}
 
-	async runSequentially(commands: Array<Command>): Promise<void> {
+	async runSequentially(commands: Array<Command>, options?: ExecOptions): Promise<void> {
 		commands = this.formatCommand(commands);
 		for (const command of commands) {
-			await this.execute(command);
+			await this.execute(command, options);
 		}
 	}
 
-	async runParallel(commands: Array<Command>): Promise<void[]> {
+	async runParallel(commands: Array<Command>, options?: ExecOptions): Promise<void[]> {
 		commands = this.formatCommand(commands);
 		const promises = new Array<Promise<void>>();
 		for (const command of commands) {
-			promises.push(this.execute(command));
+			promises.push(this.execute(command, options));
 		}
 		return Promise.all(promises);
 	}
